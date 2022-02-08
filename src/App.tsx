d31c21d74d43474ba145e28b34b6f898b5833eda
Prashant Kurlekar <prashantkurlekar@gmail.com>
2022-02-08 23:10:18 +1100

    theme dark and light

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,23 @@ import Top from './pages/Top/Top';
 const App = () => {
   return (
     <div className="app">
-      <h2>Hacker News - React</h2>
+      <div className="app-header">
+        <h2>Hacker News - React</h2>
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Top />} />
         </Routes>
       </BrowserRouter>
-      <footer>Github - URL</footer>
+      <footer>
+        <a
+          href="https://github.com/prashantkurlekar/hackernews-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          github
+        </a>
+      </footer>
     </div>
   );
 };
