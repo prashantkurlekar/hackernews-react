484c031e6b72d03e323ee6e17b15fdd5c70349d4
Prashant Kurlekar <prashantkurlekar@gmail.com>
2022-02-09 16:57:50 +1100

    toggle theme light and dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Top from './pages/Top/Top';
 
+const light = 'light';
+const dark = 'dark';
+
 const App = () => {
+  const [theme, setTheme] = useState(light);
+
+  const switchTheme = (theme: string) => document.documentElement.setAttribute('data-theme', theme);
+
+  const toggleTheme = () => (theme === light ? setTheme(dark) : setTheme(light));
+
+  useEffect(() => {
+    switchTheme(theme);
+  }, [theme]);
+
   return (
     <div className="app">
       <div className="app-header">
@@ -20,6 +34,7 @@ const App = () => {
         >
           github
         </a>
+        <button onClick={toggleTheme}>Toggle Theme</button>
       </footer>
     </div>
   );
